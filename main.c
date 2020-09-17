d333344254d1125294501116c33eaf62d03e698b
ClaraCastaD <1980@holbertonschool.com>
2020-09-17 21:02:24 +0000

    Adding a space in error message

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	var.fd = fopen(argv[1], "r");
 	if (!var.fd)
 	{
-		fprintf(stderr, "Error: Can't open file%s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (getline(&var.buff, &lenght, var.fd) != EOF)
