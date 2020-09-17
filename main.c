9cdfdeb4c6676b6d08eb5074fddee63946ad1bf1
Villasdaniel <2057@holbertonschool.com>
2020-09-17 15:37:53 -0500

    fix main with some different conditionals

--- main.c
+++ main.c
@@ -15,18 +15,15 @@ int main(int argc, char *argv[])
 	unsigned int count_line = 0;
 	void (*operation)(stack_t **stack, unsigned int line_number) = NULL;
 
-	if (argc >= 2)
-	{
-		var.fd = fopen(argv[1], "r");
-		if (!var.fd)
-		{
-			fprintf(stderr, "Error: Can't open file%s\n", argv[1]);
-			exit(EXIT_FAILURE);
-		}
-	}
-	if (argc < 2)
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	var.fd = fopen(argv[1], "r");
+	if (!var.fd)
+	{
+		fprintf(stderr, "Error: Can't open file%s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (getline(&var.buff, &lenght, var.fd) != EOF)
