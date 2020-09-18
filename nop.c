55ae190d7449c5e5ac025ef1df67b848c2b0e7cf
ClaraCastaD <1980@holbertonschool.com>
2020-09-18 03:28:32 +0000

    Adding nop function

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop - do anything
+ * @node: is the element
+ * @line_number: line number
+ */
+void nop(stack_t **node, unsigned int line_number)
+{
+    (void)node;
+    (void)line_number;
+}
