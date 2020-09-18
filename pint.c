352da8957dbd3a47a077a5ba3dca9c43ff954712
Villasdaniel <2057@holbertonschool.com>
2020-09-17 20:06:13 -0500

    pint, print number add in the las line

---
+++ pint.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * pint - print the numbers add
+ * @node: is the element
+ * @line_number: line number
+ */
+void pint(stack_t **node, unsigned int line_number)
+{
+	stack_t *node_pall = *node;
+
+	if (!node)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty", line_number);
+		free(var.buff);
+		free_stack(*node);
+		fclose(var.fd);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", node_pall->n);
+}
