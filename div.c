7e8ac0b2f431acb980f64fac468fea76d600dec9
ClaraCastaD <1980@holbertonschool.com>
2020-09-18 03:49:47 +0000

    Fixing div function

--- div.c
+++ div.c
@@ -8,12 +8,12 @@ void div(stack_t **node, unsigned int line_number)
 {
     if (*node == NULL || (*node)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 		free(var.buff);
 		free_stack(*node);
 		fclose(var.fd);
 		exit(EXIT_FAILURE);
 	}
-    (*node)->next->n = (*node)->next->n / (*node)->n;
+    (*node)->n = (*node)->next->n / (*node)->n;
 	pop(node, line_number);
 }
