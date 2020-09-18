43f9936f47074447ac547ff4845b0e6b1b962e21
ClaraCastaD <1980@holbertonschool.com>
2020-09-18 01:07:23 +0000

    Fixing mistakes in pint function

--- pint.c
+++ pint.c
@@ -8,7 +8,7 @@ void pint(stack_t **node, unsigned int line_number)
 {
 	stack_t *node_pall = *node;
 
-	if (!node)
+	if (!node_pall)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		free(var.buff);
